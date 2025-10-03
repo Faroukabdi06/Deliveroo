3bc1e887966fa796fd81f06b08c85379508e60ae
Farouk Abdi <abdifarouk58@gmail.com>
2025-10-03 16:09:21 +0300

    added frontend test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    dedupe: ['react', 'react-dom'], // <- ensures only one copy of React is used
+    dedupe: ['react', 'react-dom'],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    deps: {
+      inline: ['@testing-library/react', 'jsdom'],
+    },
   },
 })
