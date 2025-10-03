5ce76d371439cb01f28f980949f7692ea7acd226
Farouk Abdi <abdifarouk58@gmail.com>
2025-10-03 14:07:32 +0300

    written test for backend

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -34,11 +34,6 @@ def client(app):
     with app.test_client() as client:
         yield client
 
-# @pytest.fixture(autouse=True)
-# def clean_db(db):
-#     yield
-#     db.session.rollback()
-
 
 
 @pytest.fixture
